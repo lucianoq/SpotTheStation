cccb16c4e894e2638d9b5efbb3c9b6e2abfa574b
Simone Rutigliano <simonerutigliano88@gmail.com>
2014-02-21 17:46:43 +0100

    read rss

--- js/webapp.js
+++ js/webapp.js
@@ -1,4 +1,7 @@
 (function () {
+    $(document).ready(function () {
+        $('#main').rssfeed('http://spotthestation.nasa.gov/sightings/xml_files/Italy_None_Adelfia.xml');
+    });
     /*
         WebActivities:
 
